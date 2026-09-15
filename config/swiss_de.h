469626a4351e76684e71d867d9f755f797b53d65
Leon-Rossi <leon.rossi@icloud.com>
2026-09-15 17:09:02 +0200

    v3.2.3

--- config/swiss_de.h
+++ config/swiss_de.h
@@ -1,10 +1,10 @@
-// swiss_de.h
 #pragma once
 
 #include <dt-bindings/zmk/keys.h>
 
-#define CH_Z Y
-#define CH_Y Z
+// --- Letters ---
+#define CH_Z Z
+#define CH_Y Y
 #define CH_Q Q
 #define CH_W W
 #define CH_E E
@@ -30,61 +30,68 @@
 #define CH_N N
 #define CH_M M
 
-// --- Top Row Numbers & Symbols (Shift + Number) ---
+// --- Number Row ---
 #define CH_1 N1       // 1
 #define CH_2 N2       // 2
 #define CH_3 N3       // 3
-#define CH_4 N4       // 4 #define CH_5 N5       // 5 #define CH_6 N6       // 6
+#define CH_4 N4       // 4
+#define CH_5 N5       // 5
+#define CH_6 N6       // 6
 #define CH_7 N7       // 7
 #define CH_8 N8       // 8
 #define CH_9 N9       // 9
 #define CH_0 N0       // 0
 
-#define CH_SECT GRV  // §
-#define CH_QUOT MINUS // '
-#define CH_CIRC EQUAL  // ^ (dead)
-#define CH_UDIA LBRC // ü
-#define CH_DIAE RBRC // ¨ (dead)
-#define CH_ODIA SCLN // ö
-#define CH_ADIA QUOT // ä
-#define CH_DLR  KC_NUHS // $
-#define CH_LABK KC_NUBS // <
-#define CH_COMM COMMA // ,
-#define CH_DOT  DOT  // .
-#define CH_MINS SLASH // -
-#define CH_DEG  S(CH_SECT) // °
-#define CH_PLUS S(CH_1)    // +
-#define CH_DQUO S(CH_2)    // "
-#define CH_ASTR S(CH_3)    // *
-#define CH_CCED S(CH_4)    // ç
-#define CH_PERC S(CH_5)    // %
-#define CH_AMPR S(CH_6)    // &
-#define CH_SLSH S(CH_7)    // /
-#define CH_LPRN S(CH_8)    // (
-#define CH_RPRN S(CH_9)    // )
-#define CH_EQL  S(CH_0)    // =
-#define CH_QUES S(CH_QUOT) // ?
-#define CH_GRV  S(CH_CIRC) // ` (dead)
-#define CH_EGRV S(CH_UDIA) // è
-#define CH_EXLM S(CH_DIAE) // !
-#define CH_EACU S(CH_ODIA) // é
-#define CH_AGRV S(CH_ADIA) // à
-#define CH_PND  S(CH_DLR)  // £
-#define CH_RABK S(CH_LABK) // >
-#define CH_SCLN S(CH_COMM) // ;
-#define CH_COLN S(CH_DOT)  // :
-#define CH_UNDS S(CH_MINS) // _
-#define CH_BRKP ALGR(CH_1)    // ¦
-#define CH_AT   ALGR(CH_2)    // @
-#define CH_HASH ALGR(CH_3)    // #
-#define CH_NOT  ALGR(CH_6)    // ¬
-#define CH_PIPE ALGR(CH_7)    // |
-#define CH_CENT ALGR(CH_8)    // ¢
-#define CH_ACUT ALGR(CH_QUOT) // ´ (dead)
-#define CH_TILD ALGR(CH_CIRC) // ~ (dead)
-#define CH_EURO ALGR(CH_E)    // €
-#define CH_LBRC ALGR(CH_UDIA) // [
-#define CH_RBRC ALGR(CH_DIAE) // ]
-#define CH_LCBR ALGR(CH_ADIA) // {
-#define CH_RCBR ALGR(CH_DLR)  // }
-#define CH_BSLS ALGR(CH_LABK) // (backslash)
+// --- Unshifted Symbols ---
+#define CH_SECT GRV       // §
+#define CH_QUOT MINUS     // '
+#define CH_CIRC EQUAL     // ^
+#define CH_UDIA LBRC      // ü
+#define CH_DIAE RBRC      // ¨
+#define CH_ODIA SCLN      // ö
+#define CH_ADIA QUOT      // ä
+#define CH_DLR  NUHS      // $
+#define CH_LABK NUBS      // <
+#define CH_COMM COMMA     // ,
+#define CH_DOT  DOT       // .
+#define CH_MINS SLASH     // -
+
+// --- Shifted Symbols ---
+#define CH_DEG  S(CH_SECT)    // °
+#define CH_PLUS S(CH_1)       // +
+#define CH_DQUO S(CH_2)       // "
+#define CH_ASTR S(CH_3)       // *
+#define CH_CCED S(CH_4)       // ç
+#define CH_PERC S(CH_5)       // %
+#define CH_AMPR S(CH_6)       // &
+#define CH_SLSH S(CH_7)       // /
+#define CH_LPRN S(CH_8)       // (
+#define CH_RPRN S(CH_9)       // )
+#define CH_EQL  S(CH_0)       // =
+#define CH_QUES S(CH_QUOT)    // ?
+#define CH_GRV  S(CH_CIRC)    // `
+#define CH_EGRV S(CH_UDIA)    // è
+#define CH_EXLM S(CH_DIAE)    // !
+#define CH_EACU S(CH_ODIA)    // é
+#define CH_AGRV S(CH_ADIA)    // à
+#define CH_PND  S(CH_DLR)     // £
+#define CH_RABK S(CH_LABK)    // >
+#define CH_SCLN S(CH_COMM)    // ;
+#define CH_COLN S(CH_DOT)     // :
+#define CH_UNDS S(CH_MINS)    // _
+
+// --- AltGr Symbols ---
+#define CH_BRKP ALGR(CH_1)       // ¦
+#define CH_AT   ALGR(CH_2)       // @
+#define CH_HASH ALGR(CH_3)       // #
+#define CH_NOT  ALGR(CH_6)       // ¬
+#define CH_PIPE ALGR(CH_7)       // |
+#define CH_CENT ALGR(CH_8)       // ¢
+#define CH_ACUT ALGR(CH_QUOT)    // ´
+#define CH_TILD ALGR(CH_CIRC)    // ~
+#define CH_EURO ALGR(CH_E)       // €
+#define CH_LBRC ALGR(CH_UDIA)    // [
+#define CH_RBRC ALGR(CH_DIAE)    // ]
+#define CH_LCBR ALGR(CH_ADIA)    // {
+#define CH_RCBR ALGR(CH_DLR)     // }
+#define CH_BSLS ALGR(CH_LABK)    // \
